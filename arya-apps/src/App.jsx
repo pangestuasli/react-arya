0b331841c9c5f451a4fdb5d888db5850dcdb38d8
pangestuasli <pangestu24si@mahasiswa.pcr.ac.id>
2026-03-09 15:24:29 +0700

    Pertemuan2

---
+++ arya-apps/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(7)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>P</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Cukup Tau
+      </p>
+    </>
+  )
+}
+
+export default App
